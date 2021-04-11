724d40e73dbf88e1c5805e1ab7d836a6b6aba468
kumass2020 <kumass2021@gmail.com>
2021-04-12 04:19:24 +0900

    Create Back-Propagation logic

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -142,6 +142,7 @@ sigmoid2 = Sigmoid()
 offset = 0
 W = 1
 eta = 0.1
+bias = 0.3
 X, D, W1, W2 = init_network()
 epoch = 0
 
@@ -149,9 +150,9 @@ epoch = 0
 delta1 = np.array([0, 0, 0, 0, 0])
 # 출력층 오차
 delta2 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-A = []
-Z = []
-O = []
+A = [0 for i in range(5)]
+Z = [0 for i in range(5)]
+O = [0 for i in range(10)]
 
 test = X[0]
 test = test.flatten()
@@ -159,26 +160,38 @@ test = test.flatten()
 while epoch < 10000:
     print("epoch: " + str(epoch))
     for i in range(10):  # ㄱ, ㄴ, ㄷ, ...
+        print("i: " + str(i))
         for k in range(5):  # A[0], A[1], ...
             print("k: " + str(k))
             test4 = X[i].flatten()
             test5 = W1[:, k]
-            A.insert(k, np.dot(X[i].flatten(), W1[:, k]))
+            # A.insert(k, np.dot(X[i].flatten(), W1[:, k]))
+            A[k] = np.dot(X[i].flatten(), W1[:, k])
+            if k == 0:
+                A[k] -= 20
             print(A[0].shape)
             print(type(A[0]))
             print(type(Z))
             print("")
+
             # test1 = sigmoid.forward(A[k])
-            Z.insert(k, sigmoid1.forward(A[k]))
+
+            # Z.insert(k, sigmoid1.forward(A[k]))
+            Z[k] = sigmoid1.forward(A[k])
+
             # Z = np.array(Z).flatten()
         test3 = Z
         for j in range(10):
             print("j: " + str(j))
             test1 = np.array(Z).flatten()
             test2 = W2[:, j]
-            O.insert(j, np.dot(np.array(Z).flatten(), W2[:, j]))
+            # O.insert(j, np.dot(np.array(Z).flatten(), W2[:, j]))
+            O[j] = np.dot(np.array(Z).flatten(), W2[:, j])
             O[j] = sigmoid2.forward(O[j])
-            delta2[j] = O[j] * (1 - O[j]) * (1 - O[j])
+            test6 = O[j]
+            test7 = 1 - O[j]
+            test8 = (D[i])[j] - O[j]
+            delta2[j] = O[j] * (1 - O[j]) * ((D[i])[j] - O[j])
         for m in range(5):
             print("m: " + str(m))
             summ = 0
@@ -186,6 +199,12 @@ while epoch < 10000:
                 print("n: " + str(n))
                 summ += delta2[n] * W2[m][n]
             delta1[m] = Z[m] * (1 - Z[m]) * summ
+        for n in range(5):
+            for o in range(10):
+                W2[n][o] = W2[n][o] + eta * delta2[o] * Z[n]
+        for k in range(64):
+            for j in range(5):
+                W1[k][j] = W1[k][j] + eta * delta1[j] * (X[i]).flatten()[k]
 
     print("")
     epoch += 1
